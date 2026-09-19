6312bc7ce08f71cb86b86bb7778e560f26682010
Chresh <serge.berger999@yahoo.de>
2026-09-19 09:20:45 +0200

    removed bat file

--- runManual.bat
+++
@@ -1,2 +0,0 @@
-powershell -NoProfile -Command "& { Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned; & .venv\Scripts\Activate.ps1; $env:PYTHONIOENCODING='utf-8'; py main.py --manual }"
-pause
